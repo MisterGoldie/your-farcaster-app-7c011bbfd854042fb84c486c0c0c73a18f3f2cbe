8c689f46d927d3853836d9be335aad93f6146d09
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-12-03 08:06:13 -0600

    sdbdabaes

--- src/app/api/frame/route.ts
+++ src/app/api/frame/route.ts
@@ -2,14 +2,15 @@ import { NextRequest } from 'next/server'
 
 export async function GET(req: NextRequest) {
   const frameMetadata = {
+    version: 'vNext',
+    image: `${process.env.NEXT_PUBLIC_URL}/game-preview.png`,
     buttons: [
       {
         label: "Start Game",
         action: "post"
       }
     ],
-    image: `${process.env.NEXT_PUBLIC_URL}/game-preview.png`,
-    post_url: `${process.env.NEXT_PUBLIC_URL}/api/frame`
+    postUrl: `${process.env.NEXT_PUBLIC_URL}/api/frame`
   }
 
   return new Response(
@@ -19,9 +20,10 @@ export async function GET(req: NextRequest) {
         <title>POD Play Tic-Tac-Toe</title>
         <meta property="fc:frame" content="vNext" />
         <meta property="fc:frame:image" content="${frameMetadata.image}" />
+        <meta property="fc:frame:image:aspect_ratio" content="1.91:1" />
         <meta property="fc:frame:button:1" content="${frameMetadata.buttons[0].label}" />
         <meta property="fc:frame:button:1:action" content="${frameMetadata.buttons[0].action}" />
-        <meta property="fc:frame:post_url" content="${frameMetadata.post_url}" />
+        <meta property="fc:frame:post_url" content="${frameMetadata.postUrl}" />
       </head>
     </html>`,
     {
