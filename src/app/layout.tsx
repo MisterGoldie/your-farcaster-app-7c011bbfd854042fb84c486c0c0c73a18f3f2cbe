9709db22d33973f878911b5b5f063d5094e4e22b
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-12-05 03:07:14 -0600

    hjvjvhvhj

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
         action: {
           type: "launch_frame",
           name: "POD Play",
-          url: "https://podplayv2.vercel.app/howtoplay",
+          url: "https://podplayv2.vercel.app/",
           splashImageUrl: `${process.env.NEXT_PUBLIC_URL}/splash.png`,
           splashBackgroundColor: "#9333ea"
         }
